02f5b83aeb6218ac6ea2d78defaff0b2087fb9f4
Pontus Karlsson <next557@outlook.com>
2020-11-17 12:16:55 +0100

    Add files via upload

---
+++ EasyMD5.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace cameratek
+{
+    class EasyMD5
+    {
+        private static string GetMd5Hash(byte[] data)
+        {
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+            return sBuilder.ToString();
+        }
+        public static string Hash(string data)
+        {
+            using (var md5 = MD5.Create())
+                return GetMd5Hash(md5.ComputeHash(Encoding.UTF8.GetBytes(data)));
+        }
+    }
+}
